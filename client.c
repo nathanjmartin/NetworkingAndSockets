4ffb0bd89aa2033553d256e1dc1b9384cad6792b
Nathan Martin <nathanm9000@gmail.com>
2019-08-05 21:19:04 -0500

    Created basic C client program

--- client.c
+++ client.c
@@ -1,3 +1,50 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define MAX 80
+#define PORT 12345
+#define SA struct sockaddr
+
 int main(void) {
+    int sockfd, connfd;
+    char *hello = "Hello, I am the client";
+    struct sockaddr_in servaddr, cli;
+
+    //create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // check if socket created
+    if(sockfd == -1) {
+        printf("socket creation failure");
+        exit(0);
+    }
+
+    //else socket created successfully
+    else {
+        printf("socket creation successful\n");
+    }
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assigning ip address and port
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    //connect the client socket to the server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection failed");
+        exit(0);
+    }
+
+    else
+    {
+        printf("connected to python server\n");
+    }
+
+    send(sockfd, hello, strlen(hello), 0);
+    close(sockfd);
     
 }
---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+
+# variables
+port = 12345
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.bind(('', port))
+socket.listen(5)
+
+while True:
+    conn, address = socket.accept()
+    from_client = ''
+    
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            break
+        from_client += data
+        print from_client
+        conn.send("Response from server\n")
+        
+    conn.close()
+    print 'client disconnected'
+    break
