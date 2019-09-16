4ab07294df83e6d0190382c9bb931634828d6a92
nathanjmartin <nathanm9000@gmail.com>
2019-09-16 12:46:18 -0500

    Current push

--- server.py
+++ server.py
@@ -1,35 +1,47 @@
 import socket
 import subprocess
 import os
+import sys 
 
 # variables
 port = 12345
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# listen on localhost
 socket.bind(('127.0.0.1', port))
 socket.listen(5)
 
+# while listening
 while True:
     conn, address = socket.accept()
     from_client = ''
     
+    # while receiving
     while True:
         data = conn.recv(4096)
         if not data:
             break
+
+        # append data to from_client
         from_client += data
         
-        # take from_client and pass into subprocess to execute a command
-        # and return that output
-        # how can i replace 'ls' with 'from_client' without getting null characters error
+        # pass the command from_client into os.popen
         output = os.popen(from_client).read()
+
+        # print the command from_client
         print(from_client)
+
+        # if touch is in from client, make output success since there is no output from touch command
         if 'touch' in from_client:
             output = 'Success!'
+
+        # send the output back to the client
         conn.send(output)
-        #output = 'Success!'
-        from_client = ''
         
+        # reset from client to an empty string
+        from_client = ''
+
+    # close connection
     conn.close()
     print 'client disconnected'
     break
